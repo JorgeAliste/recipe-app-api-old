7f34c6ab2de2d9869db760337d952ed33ccfc831
Jorge Aliste <j.alistea@gmail.com>
2022-01-31 18:34:34 -0300

    Added image field to recipe model.

--- app/app/settings.py
+++ app/app/settings.py
@@ -122,6 +122,11 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
